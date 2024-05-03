8fe72d12f305db50e11b92cc1f98dfb51dbd3a0f
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-03 18:20:49 -0300

    [main] => feat: first commit

---
+++ src/test/java/br/com/thomasquinaia/sendemailapi/SendEmailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thomasquinaia.sendemailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
