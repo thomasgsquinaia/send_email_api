adb602ffb8f655dc6979a613c186c3eee90e86a2
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-04 11:52:47 -0300

    [main] => feat: email controller

--- src/main/java/br/com/thomasquinaia/sendemailapi/EmailController.java
+++ src/main/java/br/com/thomasquinaia/sendemailapi/EmailController.java
@@ -1,5 +1,18 @@
 package br.com.thomasquinaia.sendemailapi;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("email")
 public class EmailController {
+    private final EmailService emailService;
+
+    @PostMapping
+    public void sendEMail(Email email) {
+        System.out.println(email);
+    }
     
 }
