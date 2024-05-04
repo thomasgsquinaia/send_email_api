715910a07cd076093c30317dcb2fc95e85671871
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-04 11:34:16 -0300

    [main] => feat: fix version java

---
+++ src/main/java/br/com/thomasquinaia/sendemailapi/Email.java
@@ -0,0 +1,5 @@
+package br.com.thomasquinaia.sendemailapi;
+
+public record Email(String to, String subject, String body) {
+    
+}
---
+++ src/main/java/br/com/thomasquinaia/sendemailapi/EmailService.java
@@ -0,0 +1,5 @@
+package br.com.thomasquinaia.sendemailapi;
+
+public record EmailService(String to, String subject, String body) {
+    
+}
